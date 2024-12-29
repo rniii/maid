20c56ce0e6ccc6653c40ad651e2671c6d64d6399
rini <rini@rinici.de>
2024-12-29 17:14:58 -0300

    waow
    Co-authored-by: 87 <87flowers@noreply.codeberg.org>

--- src/main.zig
+++ src/main.zig
@@ -1,10 +1,20 @@
-const std = @import("std");
-const Parser = @import("parser.zig");
+pub const Task = struct {
+    name: []u8,
+    description: []u8,
+    code: struct { lang: []u8, text: []u8 },
+};
 
 pub fn main() !void {
     const alloc = std.heap.page_allocator;
     const stdin = try std.fs.cwd().openFile("README.md", .{});
 
     var parser = try Parser.init(alloc);
-    parser.parse(stdin.reader());
+    const tasks = try parser.parse(alloc, stdin.reader());
+
+    for (tasks.items) |task| {
+        std.debug.print("{s}\n", .{task.name});
+    }
 }
+
+const std = @import("std");
+const Parser = @import("parser.zig");
