007b0eb995b9479f138b040d986d1c47392ffbc3
rini <rini@rinici.de>
2024-12-30 17:04:43 -0300

    a

--- src/Parser.zig
+++ src/Parser.zig
@@ -1,128 +1,117 @@
-buffer: std.ArrayList(u8),
+line: std.ArrayList(u8),
+block: std.ArrayList(u8),
+
 headingNr: usize = 0,
 fenceNr: usize = 0,
 lineNr: usize = 0,
-blank: bool = true,
+lang: ?Task.Lang = null,
 
 pub fn init(alloc: std.mem.Allocator) !Self {
-    const buffer = try std.ArrayList(u8).initCapacity(alloc, 32);
-    return .{ .buffer = buffer };
+    const line = try std.ArrayList(u8).initCapacity(alloc, 32);
+    const block = try std.ArrayList(u8).initCapacity(alloc, 128);
+
+    return .{ .line = line, .block = block };
 }
 
 pub fn parse(self: *Self, alloc: std.mem.Allocator, file: anytype) !std.ArrayList(Task) {
     var tasksHeading: usize = 0;
-    var state: enum(u8) { tasklist, magic, task, desc, code } = .tasklist;
+    var state: enum { tasklist, magic, task, desc, code } = .tasklist;
 
     var tasks = std.ArrayList(Task).init(alloc);
     var task: Task = undefined;
 
-    while (true) {
-        self.nextBlock(file) catch break;
+    while (self.nextBlock(file) catch null) |_| sw: switch (state) {
+        .tasklist => {
+            if (self.headingNr == 0) continue;
 
-        std.debug.print("{d} {:3} | {s}\n", .{ @intFromEnum(state), self.lineNr, self.buffer.items });
+            state = .magic;
+            tasksHeading = self.headingNr;
+        },
+        .magic => {
+            state = if (isMagic(self.block.items)) .task else .tasklist;
+        },
+        .task => {
+            if (self.headingNr == 0) continue;
+            if (self.headingNr <= tasksHeading) break;
 
-        sw: switch (state) {
-            .tasklist => {
-                if (self.headingNr == 0) continue;
+            state = .desc;
+            task.name = try std.ascii.allocLowerString(alloc, trim(u8, self.block.items[self.headingNr..], " \t"));
+        },
+        .desc => {
+            if (self.headingNr > 0) continue :sw .task;
 
-                state = .magic;
-                tasksHeading = self.headingNr;
-            },
-            .magic => {
-                if (self.blank) continue;
-
-                state = if (isMagic(self.buffer.items)) .task else .tasklist;
-            },
-            .task => {
-                if (self.headingNr == 0) continue;
-                if (self.headingNr <= tasksHeading) break;
-
-                state = .desc;
-                task.name = try std.ascii.allocLowerString(
-                    alloc,
-                    std.mem.trim(u8, self.buffer.items[self.headingNr..], " \t"),
-                );
-            },
-            .desc => {
-                if (self.headingNr > 0) continue :sw .task;
-                if (self.blank) continue;
-
-                state = .code;
-                if (self.fenceNr == 0) {
-                    var desc = try std.ArrayList(u8).initCapacity(alloc, self.buffer.items.len);
-                    while (!isBlank(self.buffer.items)) {
-                        try desc.appendSlice(self.buffer.items);
-                        try desc.append('\n');
-                        try self.nextLine(file);
-                    }
-                    self.identify();
-                    task.description = try desc.toOwnedSlice();
-                } else {
-                    task.description = try alloc.dupe(u8, "[No description]");
-                }
+            state = .code;
+            if (self.fenceNr == 0) {
+                task.desc = try alloc.dupe(u8, trim(u8, self.block.items, " \t"));
+            } else {
+                task.desc = try alloc.dupe(u8, "[No description]");
                 continue :sw state;
-            },
-            .code => {
-                if (self.headingNr > 0) continue :sw .task;
-                if (self.blank or self.fenceNr == 0) continue;
+            }
+        },
+        .code => {
+            if (self.headingNr > 0) continue :sw .task;
+            if (self.fenceNr == 0) continue;
 
-                state = .task;
-                task.code.lang = try alloc.dupe(u8, self.buffer.items[self.fenceNr..]);
-                {
-                    var code = try std.ArrayList(u8).initCapacity(alloc, self.buffer.items.len);
-                    while (fence(self.buffer.items) < self.fenceNr) {
-                        try code.appendSlice(self.buffer.items);
-                        try code.append('\n');
-                        try self.nextLine(file);
-                    }
-                    self.identify();
-                    task.code.text = try code.toOwnedSlice();
-                }
-                try tasks.append(task);
-            },
-        }
-    }
+            state = .task;
+            task.code.lang = self.lang.?;
+            task.code.text = try alloc.dupe(u8, trim(u8, self.block.items, "\n"));
+            self.nextBlock(file) catch {};
+            try tasks.append(task);
+            continue :sw state;
+        },
+    };
 
     return tasks;
 }
 
-fn identify(self: *Self) void {
-    self.headingNr = heading(self.buffer.items);
-    self.fenceNr = fence(self.buffer.items);
-    self.blank = isBlank(self.buffer.items);
-}
-
 fn nextBlock(self: *Self, file: anytype) !void {
-    try self.nextLine(file);
+    self.block.clearRetainingCapacity();
 
-    if (self.fenceNr > 0) {
-        while (true) {
-            try self.nextLine(file);
-            if (fence(self.buffer.items) >= self.fenceNr) break;
-        }
+    while (true) {
         try self.nextLine(file);
-    } else if (!self.blank and self.headingNr == 0) {
-        while (true) {
-            if (isBlank(self.buffer.items)) break;
-            try self.nextLine(file);
-        }
+        if (!isBlank(self.line.items)) break;
     }
 
-    self.identify();
+    self.headingNr = heading(self.line.items);
+    self.fenceNr = fence(self.line.items);
+
+    if (self.headingNr > 0) {
+        try self.block.appendSlice(self.line.items);
+    } else if (self.fenceNr > 0) {
+        self.lang = Task.Lang.fromId(trim(u8, self.line.items[self.fenceNr..], " \t"));
+
+        while (true) : ({
+            try self.block.append('\n');
+            try self.block.appendSlice(self.line.items);
+        }) {
+            try self.nextLine(file);
+            if (fence(self.line.items) >= self.fenceNr) break;
+        }
+    } else {
+        try self.block.appendSlice(self.line.items);
+
+        while (true) : ({
+            try self.block.append(' ');
+            try self.block.appendSlice(self.line.items);
+        }) {
+            try self.nextLine(file);
+            if (isBlank(self.line.items)) break;
+        }
+    }
 }
 
 fn nextLine(self: *Self, file: anytype) !void {
-    self.buffer.clearRetainingCapacity();
+    self.line.clearRetainingCapacity();
 
-    file.streamUntilDelimiter(self.buffer.writer(), '\n', null) catch |err| {
-        if (self.buffer.items.len == 0) return err;
+    file.streamUntilDelimiter(self.line.writer(), '\n', null) catch |err| {
+        if (self.line.items.len == 0) return err;
     };
 
     self.lineNr += 1;
 }
 
 fn isMagic(text: []u8) bool {
-    return std.mem.eql(u8, text, "<!-- maid-tasks -->");
+    return std.mem.startsWith(u8, text, "<!-- maid-tasks -->");
 }
 
 fn isBlank(text: []u8) bool {
@@ -152,5 +141,6 @@ fn heading(text: []u8) usize {
 }
 
 const std = @import("std");
+const trim = std.mem.trim;
 const Self = @This();
-const Task = @import("main.zig").Task;
+const Task = @import("Task.zig");
--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,3 @@
-pub const Task = struct {
-    name: []u8,
-    description: []u8,
-    code: struct { lang: []u8, text: []u8 },
-};
-
 pub fn main() !void {
     const alloc = std.heap.page_allocator;
     const stdin = try std.fs.cwd().openFile("README.md", .{});
@@ -12,12 +6,8 @@ pub fn main() !void {
     const tasks = try parser.parse(alloc, stdin.reader());
 
     for (tasks.items) |task| {
-        std.debug.print("task: {s}\n", .{task.name});
-        std.debug.print("{s}\n", .{task.description});
-        std.debug.print("\n", .{});
-        std.debug.print("code({s}):\n", .{task.code.lang});
-        std.debug.print("{s}\n", .{task.code.text});
-        std.debug.print("\n", .{});
+        std.debug.print("{s}\n", .{task.name});
+        std.debug.print("  {s}\n\n", .{task.desc});
     }
 }
 
